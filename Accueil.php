feda0d52ad4f580a88b39de690997c44f3d82602
Matteo Trabelsi <mtrabelsi@sio.local>
2022-01-06 10:41:58 +0100

    restructuration

---
+++ Accueil.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Accueil</title>
+  </head>
+  <body>
+    <center>
+          <br>
+          <br>
+          <br>
+          <div id="centrer">
+            <h1>Bienvenue sur mon Portfoliot</h1>
+            <h2><U>"Dans la vie on ne fait pas ce que l'on veut mais on est responsable de ce que l'on est."</U>
+            <h2>Actuellement en BTS SIO en alternance, Passionné d'informatique toujours à la recherche du progrès.</h2>
+            <img src="image/OIP.jfif" />
+          </div>
+        </br>
+        </br>
+        </br>
+  </center>
+  <?php include "index.php";
+  ?>
+  </body>
+</html>
---
+++ Expérience.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Expérience</title>
+
+  </head>
+  <body>
+    <?php include "Index.php";
+    ?>
+          <center>
+                <div id="centrer">
+                  <br><br><br><h1>Expérience</h1></br></br></br>
+                  <div id="décalagauche">
+                    <h2><U><strong>Poste occupé</strong></U>
+                    <br>Apprentie Informaticien</br></h2>
+                    <h2><U><strong> Entreprise: AF Maintenance</strong></U>
+                      <br>Situé à Mouen</br> </h2>
+                  </div>
+
+
+                  <p>Réalisation: Je m'occupe de l'apprentissage des demandes informatique de l'entreprise.</p>
+                  <p>Date de début : 28 Aout 2021 jusqu'au 28 Aout 2023 .<p>
+                  <p> La durée est de deux ans car j'effectue l'alternance en tant que BTS SIO.<p>
+                    <li><a href='documentpdf/Mon CV.pdf'>Mon CV</a></li>
+                  </div>
+          </center>
+
+  </body>
+</html>
